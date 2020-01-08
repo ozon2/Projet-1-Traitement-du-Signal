ae0e886c4220656816c6ae6943f92c1035066336
Grunig Axel <agrunig@titane.enseeiht.fr>
2020-01-08 10:15:47 +0100

    Fin du filtrage, partie 4.1.3

--- FICHIERS_DVBRCS/MF_TDMA.m
+++ FICHIERS_DVBRCS/MF_TDMA.m
@@ -42,12 +42,12 @@ Sm2 = abs(M2).^2;
 f = linspace(-Fe/2, Fe/2, length(M1));
 figure; subplot(2,1,1); semilogy(f, fftshift(Sm1));
 title("DSP de m1");
-xlabel("Fréquence (f)")
+xlabel("Fréquence (Hz)")
 ylabel("Amplitude Sm1(t)");
 
 subplot(2,1,2); semilogy(f, fftshift(Sm2));
 title("DSP de m2");
-xlabel("Frequence (f)")
+xlabel("Frequence (Hz)")
 ylabel("Amplitude Sm2(t)");
 
 %% 3.2.2 Construction du signal MF-TDMA
@@ -59,6 +59,8 @@ Signal1(N+1:2*N) = m1;
 
 Signal2 = zeros (1,5*N);
 Signal2(4*N+1:5*N) = m2;
+
+NP = 2^nextpow2(length(Signal1));
 
 % Tracé des signaux
 t = linspace(0, 5*Tslot, length(Signal1));
@@ -92,17 +94,92 @@ xlabel("Temps (s)");
 ylabel("Amplitude");
 
 % Calcul de la transformée de fourier de x
-X =fft(x);
+X =fft(x, NP);
 
 % Calcul de la DSP de x
 Sx = abs(X).^2;
 
 % Tracé de la DSP de x
-f = linspace(-Fe/2, Fe/2, length(X));
-figure; semilogy(f, fftshift(X));
+f = linspace(-Fe/2, Fe/2, length(Sx));
+figure; semilogy(f, fftshift(abs(Sx)));
 title("DSP de x");
-xlabel("Fréquence (f)")
+xlabel("Fréquence (Hz)")
 ylabel("Amplitude Sx(f)");
+
+%% 4.1.1 Synthèse du filtre passe-bas
+
+% Création filtre passe bas et TF
+fc = f2/2;
+N = 101;
+k = (-(N-1)/2 : (N-1)/2);
+filtre_bas = 2*(fc/Fe)*sinc(2*k*(fc/Fe));
+TF_filtre_bas = fft(filtre_bas, NP);
+
+% Tracé de la réponse impulsionnelle
+figure;
+plot (k,filtre_bas);
+title("Tracé de la réponse impulsionnelle du filtre bas");
+xlabel("Temps (s)");
+ylabel("Réponse impulsionnelle");
+
+% Tracé de la réponse en fréquence
+figure; 
+semilogy (linspace(-Fe/2, Fe/2, length(TF_filtre_bas)), fftshift(abs(TF_filtre_bas)));
+title("Tracé de la réponse en fréquence du filtre bas");
+xlabel("Frequence (Hz)");
+ylabel("Réponse en fréquence");
+
+% Verification du filtre
+Sxnormalise = ((1/max(abs(Sx))) * abs(Sx));
+figure;
+semilogy (linspace(-Fe/2, Fe/2, length(TF_filtre_bas)), fftshift(abs(TF_filtre_bas))); hold;
+semilogy (f, fftshift(abs(Sxnormalise)));
+title("Verification du filtre passe-bas");
+xlabel("Frequence (Hz)");
+ylabel("Amplitude")
+legend("Filtre passe-bas","DSP Normalisée")
+
+%% 4.1.2 Synthèse du ltre passe-haut
+
+% Création filtre passe haut et TF
+filtre_haut = -filtre_bas;
+filtre_haut((N-1)/2+1) = 1 - filtre_bas((N-1)/2+1);
+TF_filtre_haut = fft(filtre_haut, NP);
+
+% Tracé de la réponse impulsionnelle
+figure;
+plot (k,filtre_haut);
+title("Tracé de la réponse impulsionnelle du filtre haut");
+xlabel("Temps (s)");
+ylabel("Réponse impulsionnelle");
+
+% Tracé de la réponse en fréquence
+figure; 
+semilogy (linspace(-Fe/2, Fe/2, length(TF_filtre_haut)), fftshift(abs(TF_filtre_haut)));
+title("Tracé de la réponse en fréquence du filtre haut");
+xlabel("Frequence (Hz)");
+ylabel("Réponse en fréquence");
+
+% Verification du filtre
+figure;
+semilogy (linspace(-Fe/2, Fe/2, length(TF_filtre_haut)), fftshift(abs(TF_filtre_haut))); hold;
+semilogy (f, fftshift(abs(Sxnormalise)));
+title("Verification du filtre passe-haut");
+xlabel("Frequence (Hz)");
+ylabel("Amplitude")
+legend("Filtre passe-bas","DSP Normalisée")
+
+%% 4.2 Retour en bande de base
+
+
+
+
+
+
+
+
+
+
 
 
 
